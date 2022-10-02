054cf3b20b7a67cbabae59b3c9248e3e0ce34f11
nana-hoka <g19909hn@gm.tsuda.ac.jp>
2022-10-02 22:14:20 +0900

    add syllabus comment database

--- tsuda/admin.py
+++ tsuda/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Post
 from .models import Classroom
 from .models import Syllabus
+from .models import SyllabusComment
 
 admin.site.register(Post)
 admin.site.register(Classroom)
 admin.site.register(Syllabus)
+admin.site.register(SyllabusComment)
