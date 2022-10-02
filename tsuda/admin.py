22886c7da7974b72514fe8583711d7780df03ed0
NatsukiKomatsu <g19917kn@gm.tsuda.ac.jp>
2022-10-02 21:05:51 +0900

    add Syllabus database

--- tsuda/admin.py
+++ tsuda/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Post
 from .models import Classroom
+from .models import Syllabus
 
 admin.site.register(Post)
 admin.site.register(Classroom)
+admin.site.register(Syllabus)
